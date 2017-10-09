34e19eb9b0cb7afe36228de3202e9645eaf53690
Septdir <septdir@gmail.com>
2017-10-09 13:28:53 +0300

    Base install

---
+++ tmpcleaner.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package    JZ Cron - TmpCleaner Plugin
+ * @version    1.0.0
+ * @author     Nerudas  - nerudas.ru
+ * @copyright  Copyright (c) 2013 - 2017 Nerudas. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://nerudas.ru
+ */
+
+defined('_JEXEC') or die;
+
